e4f6db8d5d0ce431e9a03bff9f2a094be776024c
Diethart Belgard <diethart19@gmail.com>
2016-05-19 01:17:41 +0300

    Убрал логи и поправил db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,14 @@
 #require 'nokogiri'
 #require 'open-uri'
 
-#doc = Nokogiri::HTML(open('http://www.learnathome.ru/blog/100-beautiful-words'))
+require 'open-uri'
+url = "http://www.languagedaily.com/learn-german/vocabulary/common-german-words"
 
-#doc.search('//table/tbody/tr').each do |row|
-#  original = row.search('td[2]/p')[0].content.downcase
-#  translated = row.search('td[1]/p')[0].content.downcase
-#  Card.create(original_text: original, translated_text: translated, user_id: 17)
-#end
+page = Nokogiri::HTML(open(url))
+
+words = Hash[page.css('td.bigLetter').map { |original_word| original_word.content }.zip(page.css('td.bigLetter + td').map { |translated_word| translated_word.content })]
+test_user = User.create( email: "diethart19@gmail.com", password: "admin", password_confirmation: "admin" )
+test_block = Block.create( title: "testdeck", user_id: test_user.id )
+words.each_pair do |original, translated|
+  Card.create(original_text: original, translated_text: translated, user_id: test_user.id, block_id: test_block.id)
+end
